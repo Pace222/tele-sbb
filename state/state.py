e5efa3c18c60843905d951d2d3b5d724f36ca97a
Léo Theytaz <leo.theytaz@epfl.ch>
2023-12-02 17:03:40 +0100

    db v0.1 (return values in state_test.py)

--- state/state.py
+++ state/state.py
@@ -2,72 +2,77 @@ import sqlite3
 
 
 def init_db():
-    with sqlite3.connect('state.db') as con:
+    with sqlite3.connect('state.sqlite') as con:
         # Journey reference
-        con.execute("CREATE TABLE IF NOT EXISTS journeys (journey_id INTEGER PRIMARY KEY, title TEXT, destination TEXT,"
-                    "deadline TEXT, plan TEXT)")
+        con.execute("CREATE TABLE IF NOT EXISTS journeys (journey_id TINYTEXT PRIMARY KEY, title TINYTEXT,"
+                    "destination TINYTEXT, deadline TINYTEXT, plan MEDIUMTEXT)")
 
         # Telegram user ID (if known)
-        con.execute("CREATE TABLE IF NOT EXISTS users (user_id TEXT PRIMARY KEY)")
+        con.execute("CREATE TABLE IF NOT EXISTS users (user_id TINYTEXT PRIMARY KEY)")
 
         # For a journey, a user takes part from a start location, with a car (optional) of a certain capacity
-        con.execute("CREATE TABLE IF NOT EXISTS user_journeys (user_id TEXT, journey_id INTEGER,"
+        con.execute("CREATE TABLE IF NOT EXISTS user_journeys (user_id TINYTEXT, journey_id TINYTEXT,"
+                    "location TINYTEXT, car BOOLEAN NOT NULL CHECK (car IN (0, 1)),"
+                    "car_capacity INTEGER CHECK (car_capacity > 0),"
+                    "PRIMARY KEY (user_id, journey_id),"
                     "FOREIGN KEY(user_id) REFERENCES users(user_id),"
-                    "FOREIGN KEY(journey_id) REFERENCES journeys(journey_id),"
-                    "location TEXT,"
-                    "car BOOLEAN NOT NULL CHECK (car IN (0, 1)), car_capacity INTEGER CHECK (car_capacity > 0))")
+                    "FOREIGN KEY(journey_id) REFERENCES journeys(journey_id))")
 
 
 # IDEA: Generate journey_id from the telegram group ID
-def add_journey(journey_id: str, journey_text: str, destination: str, deadline: str):
-    with sqlite3.connect('state.db') as con:
-        con.execute("INSERT INTO journeys VALUES (?, ?, ?, ?)", (journey_id, journey_text, destination, deadline))
+def add_journey(journey_id: str, journey_text: str, destination: str, deadline: str, plan: str):
+    with sqlite3.connect('state.sqlite') as con:
+        con.execute("INSERT OR IGNORE INTO journeys VALUES (?, ?, ?, ?, ?)",
+                    (journey_id, journey_text, destination, deadline, plan))
 
 
 # IDEA: Generate user_id from the telegram user ID
 def add_user(user_id: str):
-    with sqlite3.connect('state.db') as con:
-        con.execute("INSERT INTO users VALUES (?) ", user_id)
+    with sqlite3.connect('state.sqlite') as con:
+        # Ignore if user already exists
+        con.execute("INSERT OR IGNORE INTO users VALUES(?)", (user_id,))
 
 
 def join_journey(user_id: str, journey_id: str, start_location, car=False,  car_capacity=1):
-    with sqlite3.connect('state.db') as con:
-        con.execute("INSERT INTO user_journeys VALUES (?, ?, ?, ?, ?)", (user_id, journey_id, start_location, car,
-                                                                         car_capacity))
+    with sqlite3.connect('state.sqlite') as con:
+        con.execute("INSERT OR IGNORE INTO users VALUES(?)", (user_id,))
+        con.execute("INSERT OR REPLACE INTO user_journeys VALUES (?, ?, ?, ?, ?)",
+                    (user_id, journey_id, start_location, car, car_capacity))
 
 
 def store_journey_plan(journey_id: str, plan: str):
-    with sqlite3.connect('state.db') as con:
+    with sqlite3.connect('state.sqlite') as con:
         con.execute("UPDATE journeys SET plan = ? WHERE journey_id = ?", (plan, journey_id))
 
 
 def get_journey(journey_id: str):
-    with sqlite3.connect('state.db') as con:
-        return con.execute("SELECT * FROM journeys WHERE journey_id = ?", journey_id).fetchone()
+    with sqlite3.connect('state.sqlite') as con:
+        return con.execute("SELECT * FROM journeys WHERE journey_id = ?", (journey_id,)).fetchone()
 
 
 def get_journey_plan(journey_id: str):
-    with sqlite3.connect('state.db') as con:
-        return con.execute("SELECT plan FROM journeys WHERE journey_id = ?", journey_id).fetchone()
+    with sqlite3.connect('state.sqlite') as con:
+        return con.execute("SELECT plan FROM journeys WHERE journey_id = ?", (journey_id,)).fetchone()
 
 
 def get_journey_users(journey_id: str):
-    with sqlite3.connect('state.db') as con:
-        return con.execute("SELECT (user_id, location, car, car_capacity) FROM user_journeys WHERE journey_id = ?", (journey_id,)).fetchall()
+    with sqlite3.connect('state.sqlite') as con:
+        return con.execute("SELECT user_id, location, car, car_capacity FROM user_journeys WHERE journey_id = ?",
+                           (journey_id,)).fetchall()
 
 
 def remove_journey(journey_id: str):
-    with sqlite3.connect('state.db') as con:
-        con.execute("DELETE FROM journeys WHERE journey_id = ?", journey_id)
-        con.execute("DELETE FROM user_journeys WHERE journey_id = ?", journey_id)
+    with sqlite3.connect('state.sqlite') as con:
+        con.execute("DELETE FROM journeys WHERE journey_id = ?", (journey_id,))
+        con.execute("DELETE FROM user_journeys WHERE journey_id = ?", (journey_id,))
 
 
 def remove_user_from_journey(user_id: str, journey_id: str):
-    with sqlite3.connect('state.db') as con:
+    with sqlite3.connect('state.sqlite') as con:
         con.execute("DELETE FROM user_journeys WHERE journey_id = ? AND user_id = ?", (journey_id, user_id))
 
 
 def remove_user(user_id: str):
-    with sqlite3.connect('state.db') as con:
-        con.execute("DELETE FROM users WHERE user_id = ?", user_id)
-        con.execute("DELETE FROM user_journeys WHERE user_id = ?", user_id)
+    with sqlite3.connect('state.sqlite') as con:
+        con.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
+        con.execute("DELETE FROM user_journeys WHERE user_id = ?", (user_id,))
---
+++ test/state_test.py
@@ -0,0 +1,26 @@
+import unittest
+import state.state as store
+
+
+class StateTest(unittest.TestCase):
+    # shows return value of store operations...
+    def test_sample_journey(self):
+        store.init_db()
+        store.add_user("test_user_1")
+        store.add_user("test_user_2")
+
+        store.add_journey("test_journey_1", "test journey 1", "test destination 1", "test deadline 1", "")
+        store.join_journey("test_user_1", "test_journey_1", "test location 1", True, 4)
+        store.join_journey("test_user_2", "test_journey_1", "test location 2", False, 1)
+        store.store_journey_plan("test_journey_1", "test plan 1")
+
+        journey = store.get_journey("test_journey_1")
+        self.assertEqual(('test_journey_1', 'test journey 1', 'test destination 1',
+                          'test deadline 1', 'test plan 1'), journey)
+
+        users = store.get_journey_users("test_journey_1")
+        self.assertEqual([('test_user_1', 'test location 1', 1, 4), ('test_user_2', 'test location 2', 0, 1)], users)
+
+
+if __name__ == '__main__':
+    unittest.main()
